c93689d5d40988b3530ac2b998d572e14321142d
Brendan Reilly <brendan.benting@gmail.com>
2019-04-19 22:42:51 -0400

    Added improved argument parser for different config files

--- gtamodel_popsyn/__main__.py
+++ gtamodel_popsyn/__main__.py
@@ -8,7 +8,16 @@ import json
 from gtamodel_popsyn.input_processor import InputProcessor
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--config', required=False, default='config.json')
+parser.add_argument('-c', '--config', action='store', required=False, default='config.json',
+                    help="Path of the configuration file to use.", type=argparse.FileType('r'))
+parser.add_argument('-p', '--preprocess-only',
+                    required=False,
+                    action="store_true",
+                    help="Only generate synthesis files and don't run synthesis procedure.")
+parser.add_argument('-o', '--output-only',
+                    required=False,
+                    action="store_true",
+                    help="Only write synthesized population from existing database data.")
 args = parser.parse_args()
 
 # initialize logger
@@ -17,7 +26,7 @@ logzero.logfile("output/pre-process.log")
 logger.info(f'GTAModel PopSyn')
 
 try:
-    config = json.load(open(args.config))
+    config = json.load(args.config)
 except:
     logger.error('Unable to load configuration file.')
     logger.info("GTAModel PopSyn will now terminate.")
@@ -31,8 +40,7 @@ gtamodel_popsyn_input_processor.generate()
 
 logger.info(f'Finished processing input data, controls and population seed records have been generated.')
 
+if args.preprocess_only:
+    sys.exit(1)
 
 logger.info('GTAModel PopSyn has completed successfully.')
-
-
-
